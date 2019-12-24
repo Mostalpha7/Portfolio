44c95aaac8582317d10ed09979f4c834df0f6215
Mustapha Abdulmujeeb <mostalpha7@gmail.com>
2019-12-23 21:38:04 -0800

    a portfolio

---
+++ app.js
@@ -0,0 +1,7 @@
+const typed = new Typed('#intro-type', {
+   strings: ['', 'Designs', ' Graphics', 'Photography', 'UI UX', 'Web development'],
+   typeSpeed: 40,
+   backSpeed: 40,
+   startDelay: 1000,
+   loop: true
+})
